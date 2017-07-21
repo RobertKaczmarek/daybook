7117c486a496d19d54bbd7bf9f29bd78d68a2e4f
Robert Kaczmarek <kaczmarek.robert95@gmail.com>
2017-07-21 22:47:00 +0200

    Merge branch 'dashboard' into 'login-signup'

---
+++ app/src/main/java/com/example/daybook/DashboardActivity.java
@@ -0,0 +1,35 @@
+package com.example.daybook;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.Calendar;
+
+public class DashboardActivity extends AppCompatActivity {
+    TextView dataTxV = (TextView) findViewById(R.id.dataTxV);
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        dataTxV.setText("Tdoay is " + Calendar.getInstance().get(Calendar.DATE) + " " + Calendar.getInstance().get(Calendar.MONTH));
+    }
+
+}
