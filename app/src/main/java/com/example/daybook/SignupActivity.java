385817444d556a27000d9f3de43db0e249752db5
Robert Kaczmarek <kaczmarek.robert95@gmail.com>
2017-07-23 15:42:57 +0200

    working signup proccess

--- app/src/main/java/com/example/daybook/SignupActivity.java
+++ app/src/main/java/com/example/daybook/SignupActivity.java
@@ -1,6 +1,7 @@
 package com.example.daybook;
 
 import android.app.ProgressDialog;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -10,11 +11,26 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 import butterknife.ButterKnife;
 import butterknife.InjectView;
 
 public class SignupActivity extends AppCompatActivity {
     private static final String TAG = "SignupActivity";
+
+    private SignupActivity.UserLoginTask mAuthTask = null;
 
     @InjectView(R.id.input_name) EditText _nameText;
     @InjectView(R.id.input_email) EditText _emailText;
@@ -63,6 +79,9 @@ public class SignupActivity extends AppCompatActivity {
         String name = _nameText.getText().toString();
         String email = _emailText.getText().toString();
         String password = _passwordText.getText().toString();
+
+        mAuthTask = new SignupActivity.UserLoginTask(name, email, password);
+        mAuthTask.execute((Void) null);
 
         // TODO: Implement your own signup logic here.
 
@@ -120,5 +139,75 @@ public class SignupActivity extends AppCompatActivity {
         }
 
         return valid;
+    }
+
+    /**
+     * Represents an asynchronous login/registration task used to authenticate
+     * the user.
+     */
+    public class UserLoginTask extends AsyncTask<Void, Void, Boolean> {
+
+        private final String mName;
+        private final String mEmail;
+        private final String mPassword;
+
+        UserLoginTask(String name, String email, String password) {
+            mName = name;
+            mEmail = email;
+            mPassword = password;
+        }
+
+        @Override
+        protected Boolean doInBackground(Void... params) {
+            JSONObject json = new JSONObject();
+            try {
+                json.put("full_name", mName);
+                json.put("email", mEmail);
+                json.put("password", mPassword);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            HttpURLConnection httpcon;
+            String url = "https://mysterious-dusk-55204.herokuapp.com/signup";
+            String data = json.toString();
+            String output = null;
+            JSONObject result = null;
+            try {
+                httpcon = (HttpURLConnection) ((new URL(url).openConnection()));
+                httpcon.setDoOutput(true);
+                httpcon.setRequestProperty("Content-Type", "application/json");
+                httpcon.setRequestProperty("Accept", "application/json");
+                httpcon.setRequestMethod("POST");
+//                httpcon.connect();
+
+                OutputStream os = httpcon.getOutputStream();
+                BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(os, "UTF-8"));
+                writer.write(data);
+                writer.close();
+                os.close();
+
+                BufferedReader br = new BufferedReader(new InputStreamReader(httpcon.getInputStream(), "UTF-8"));
+
+                String line = null;
+                StringBuilder sb = new StringBuilder();
+
+                while ((line = br.readLine()) != null) {
+                    sb.append(line);
+                }
+
+                br.close();
+                output = sb.toString();
+                result = new JSONObject(output);
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+            if (result.has("auth_token")) return true;
+            else return false;
+        }
     }
 }
