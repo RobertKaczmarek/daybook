c54e4ad144011536671bf73d4c15151a9dc969bd
Robert Kaczmarek <kaczmarek.robert95@gmail.com>
2017-08-06 12:51:48 +0200

    delete unused imports

--- app/src/main/java/com/example/daybook/EventInfoFragment.java
+++ app/src/main/java/com/example/daybook/EventInfoFragment.java
@@ -3,18 +3,11 @@ package com.example.daybook;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.TextView;
-import android.support.v4.app.FragmentActivity;
-import android.widget.Toast;
-
-import org.json.JSONException;
-import org.json.JSONObject;
 
 import static com.example.daybook.MainActivity.myEvents;
 
