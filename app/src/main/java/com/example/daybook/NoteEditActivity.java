fce5216e9aaeda3ae676aea291eec29b874d4318
Robert Kaczmarek <kaczmarek.robert95@gmail.com>
2017-08-07 18:02:14 +0200

    feature-note_edit_activity (#22)

--- app/src/main/java/com/example/daybook/NoteEditActivity.java
+++ app/src/main/java/com/example/daybook/NoteEditActivity.java
@@ -1,14 +1,11 @@
 package com.example.daybook;
 
-import android.app.DialogFragment;
 import android.content.Intent;
-import android.icu.text.SimpleDateFormat;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
-import android.widget.TextView;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -22,98 +19,61 @@ import java.io.OutputStreamWriter;
 import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.text.ParseException;
-import java.util.Date;
 
-public class EventEditActivity extends AppCompatActivity {
-    private static Event event;
+public class NoteEditActivity extends AppCompatActivity {
+    private static Note note;
 
     private JSONObject auth_token;
 
-    private static TextView dateView;
-
-    private APIUpdateTask mUpdateEventTask = null;
-
+    private APIUpdateTask mUpdateNoteTask = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_event_edit);
+        setContentView(R.layout.activity_note_edit);
 
         try {
             Intent received_intent = getIntent();
             auth_token = new JSONObject(received_intent.getStringExtra("auth_token"));
-            event = received_intent.getParcelableExtra(MainActivity.eventExtra);
+            note = received_intent.getParcelableExtra(MainActivity.noteExtra);
         } catch (JSONException e) {
             e.printStackTrace();
         }
 
-        EditText titleView = (EditText) findViewById(R.id.eventEditTitle);
-        titleView.setText(event.title);
-
-        EditText descriptionView = (EditText) findViewById(R.id.eventEditDescription);
-        descriptionView.setText(event.description);
-
-        dateView = (TextView) findViewById(R.id.eventDateEditView);
-        SimpleDateFormat dateFormat = new SimpleDateFormat("yyy-MM-dd");
-        Date event_date_fmt = null;
-        try {
-            event_date_fmt = dateFormat.parse(event.date);
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
-        dateView.setText(event_date_fmt.toString());
+        EditText descriptionView = (EditText) findViewById(R.id.noteEditDescription);
+        descriptionView.setText(note.description);
     }
 
-    public void updateEvent(View view) {
-        final EditText eventTitle = (EditText) findViewById(R.id.eventEditTitle);
-        event.title =  eventTitle.getText().toString();
+    public void updateNote(View view) {
+        final EditText noteDesc = (EditText) findViewById(R.id.noteEditDescription);
+        note.description = noteDesc.getText().toString();
 
-        final EditText eventDesc = (EditText) findViewById(R.id.eventEditDescription);
-        event.description = eventDesc.getText().toString();
-
-        mUpdateEventTask = new APIUpdateTask(event.id, event.title, event.description, event.date);
-        mUpdateEventTask.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR, (Void[])null);
+        mUpdateNoteTask = new APIUpdateTask(note.id, note.description);
+        mUpdateNoteTask.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR, (Void[])null);
     }
 
-    public void showDatePickerDialog(View v) {
-        DialogFragment newFragment = new DatePickerFragment();
-        newFragment.show(getFragmentManager(), "datePicker");
-    }
-
-    public static void setDate(Integer day, Integer month, Integer year) {
-        event.date = year + "-" + month + "-" + day;
-
-        dateView.setText(event.date);
-    }
 
     public class APIUpdateTask extends AsyncTask<Void, Void, Boolean> {
 
         private final Integer mId;
-        private final String mTitle;
         private final String mDescirption;
-        private final String mDate;
 
-        APIUpdateTask(Integer id, String title, String desc, String date) {
+        APIUpdateTask(Integer id, String desc) {
             mId = id;
-            mTitle = title;
             mDescirption = desc;
-            mDate = date;
         }
 
         @Override
         protected Boolean doInBackground(Void... params) {
             JSONObject json = new JSONObject();
             try {
-                json.put("title", mTitle);
                 json.put("description", mDescirption);
-                json.put("date", mDate);
             } catch (JSONException e) {
                 e.printStackTrace();
             }
 
             HttpURLConnection httpcon;
-            String url = "https://mysterious-dusk-55204.herokuapp.com/events/" + mId;
+            String url = "https://mysterious-dusk-55204.herokuapp.com/notes/" + mId;
             try {
                 httpcon = (HttpURLConnection) ((new URL(url).openConnection()));
                 httpcon.setDoOutput(true);
@@ -151,9 +111,9 @@ public class EventEditActivity extends AppCompatActivity {
 
         protected void onPostExecute(Boolean result) {
             Intent intent = new Intent();
-            intent.putExtra(MainActivity.eventExtra, event);
+            intent.putExtra(MainActivity.noteExtra, note);
 
-            setResult(9, intent);
+            setResult(8, intent);
             finish();
         }
     }
