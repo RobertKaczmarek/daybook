d1bc8e1ba1960a38c1f79e98e2c2661728a7e969
alinaluczak <luczak.alina@outlook.com>
2017-08-17 21:14:10 +0200

    feature-sorts (#30)

--- app/src/main/java/com/example/daybook/Alarm.java
+++ app/src/main/java/com/example/daybook/Alarm.java
@@ -3,14 +3,17 @@ package com.example.daybook;
 import android.app.PendingIntent;
 import android.os.Parcel;
 import android.os.Parcelable;
+import android.support.annotation.NonNull;
 
-import java.util.Date;
+import org.joda.time.LocalTime;
+
+import java.util.Comparator;
 
 /**
  * Created by Robert Kaczmarek on 31-Jul-17.
  */
 
-public class Alarm implements Parcelable {
+public class Alarm implements Parcelable, Comparable<Alarm> {
     public Integer id;
     public String time;
     public PendingIntent intent;
@@ -53,5 +56,17 @@ public class Alarm implements Parcelable {
 
     @Override
     public void writeToParcel(Parcel parcel, int i) {
+    }
+
+
+    public static final Comparator<Alarm> DESCENDING_COMPARATOR = new Comparator<Alarm>() {
+        public int compare(Alarm l, Alarm r) {
+            return new LocalTime(l.time).compareTo(new LocalTime(r.time));
+        }
+    };
+
+    @Override
+    public int compareTo(@NonNull Alarm alarm) {
+        return new LocalTime(this.time).compareTo(new LocalTime(alarm.time));
     }
 }
