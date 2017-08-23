0d2694216fbe98bed8fd622f7890ebc4e21229d7
Robert Kaczmarek <kaczmarek.robert95@gmail.com>
2017-08-23 23:44:39 +0200

    delete unused imports

--- app/src/main/java/com/example/daybook/MainActivity.java
+++ app/src/main/java/com/example/daybook/MainActivity.java
@@ -3,7 +3,6 @@ package com.example.daybook;
 import android.app.Activity;
 import android.app.AlarmManager;
 import android.app.PendingIntent;
-import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Rect;
@@ -14,7 +13,6 @@ import android.os.Bundle;
 import android.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.AppCompatTextView;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.MotionEvent;
@@ -35,15 +33,10 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.FileDescriptor;
-import java.io.FileOutputStream;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
-import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.Collections;
@@ -53,7 +46,6 @@ import java.util.Iterator;
 public class MainActivity extends AppCompatActivity implements DeleteDialog.NoticeDialogListener {
     final MainActivity pointer = this;
 
-    public static final String AUTH_TOKEN = "com.example.daybook.tokenFile";
     public static final String TOKEN = "token";
 
     private AlarmManager alarmManager;
